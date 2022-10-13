136dba1353b4fe0982ca199b200dcb148ef8bfc3
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-13 12:48:07 -0700

    Create contact.jsx

---
+++ multi-page-react-site/src/routes/contact.jsx
@@ -0,0 +1,14 @@
+import { Form } from "react-router-dom"
+
+export default function Contact(){
+    const contact ={
+        first: "Your",
+        last: "Name",
+        avatar: "https://placekitten.com/g/200/200",
+        twitter: "your_handle",
+        notes: "some notes",
+        favorite: true;
+    };
+
+    
+}
