00c4f16e1474036320b179a4543b2cbf89480760
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-13 10:59:18 -0700

    Update contacts.js

--- multi-page-react-site/src/contacts.js
+++ multi-page-react-site/src/contacts.js
@@ -2,6 +2,8 @@ import localforage from "localforage";
 import { matchSorter } from "match-sorter";
 import sortBy from "sort-by";
 
+// via https://reactrouter.com/en/main/start/tutorial
+
 export async function getContacts(query) {
   await fakeNetwork(`getContacts:${query}`);
   let contacts = await localforage.getItem("contacts");
