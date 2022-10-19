c82944040cb82e9e9b0215522de8d2681fc9ac2d
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-19 12:46:47 -0700

    add action to edit module

--- multi-page-react-site/src/routes/edit.jsx
+++ multi-page-react-site/src/routes/edit.jsx
@@ -2,11 +2,10 @@ import { Form, useLoaderData, redirect } from "react-router-dom";
 import { updateContact } from "../contacts";
 
 export async function action({request, params}){
-
-
-
-
-    
+    const formData = await request.formData();
+    const updates = Object.fromEntries(formData);
+    await updateContact(params.contactId, updates);
+    return redirect(`/contacts/${params.contactId}`);  
 }
 
 export default function EditContact() {
